2785a63b2bd6ce896d738c5778cf63f6e412cacf
umutertakus <umutertakus@hotmail.com>
2023-02-19 20:07:30 +0300

    Add changeQuantity function

--- src/components/CartListItem.js
+++ src/components/CartListItem.js
@@ -1,10 +1,28 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { useDispatch } from "react-redux";
+import { cartSlice } from "../store/cartSlice";
 
 const CartListItem = ({ cartItem }) => {
-  const increaseQuantity = () => {};
+  const dispatch = useDispatch();
 
-  const decreaseQuantity = () => {};
+  const increaseQuantity = () => {
+    dispatch(
+      cartSlice.actions.changeQuantity({
+        productId: cartItem.product.id,
+        amount: 1,
+      })
+    );
+  };
+
+  const decreaseQuantity = () => {
+    dispatch(
+      cartSlice.actions.changeQuantity({
+        productId: cartItem.product.id,
+        amount: -1,
+      })
+    );
+  };
 
   return (
     <View style={styles.container}>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -25,6 +25,17 @@ export const cartSlice = createSlice({
         });
       }
     },
-    changeQuantity: (state, action) => {},
+    changeQuantity: (state, action) => {
+      const { productId, amount } = action.payload;
+      const cartItem = state.items.find(
+        (item) => item.product.id === productId
+      );
+      if (cartItem) {
+        cartItem.quantity += amount;
+      }
+      if (cartItem.quantity <= 0) {
+        state.items = state.items.filter((item) => item !== cartItem);
+      }
+    },
   },
 });
