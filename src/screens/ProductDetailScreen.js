91205120b88073c0fb8224ecd79cee6ed55f0c87
umutertakus <umutertakus@hotmail.com>
2023-02-19 13:40:42 +0300

    Create ProductDetailScreen bottom box

--- src/screens/ProductDetailScreen.js
+++ src/screens/ProductDetailScreen.js
@@ -2,7 +2,10 @@ import {
   View,
   StyleSheet,
   Image,
+  Text,
+  ScrollView,
   FlatList,
+  TouchableOpacity,
   useWindowDimensions,
 } from "react-native";
 import React from "react";
@@ -11,24 +14,72 @@ import products from "../data/products";
 const ProductDetailScreen = () => {
   const product = products[0];
   const { width } = useWindowDimensions();
+
+  const addToCart = () => {
+    console.warn("Add to cart")
+  }
   return (
     <View>
-      <FlatList
-        data={product.images}
-        renderItem={({ item }) => (
-          <Image
-            source={{ uri: item }}
-            style={{ width: width, aspectRatio: 1 }}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-      />
+      <ScrollView>
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{ width: width, aspectRatio: 1 }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+
+        <View style={{ padding: 20 }}>
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>${product.price}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+      <TouchableOpacity style={styles.button} onPress={addToCart}>
+        <Text style={styles.buttonText}>Add to cart</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default ProductDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16
+  },
+});
